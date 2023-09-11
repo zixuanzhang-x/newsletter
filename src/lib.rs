979a709d2615ea6540a11db3cb728a6d237bc9b0
Zixuan Zhang <zixuanzhang.x@gmail.com>
2023-09-11 15:22:46 -0400

    Refactor to use macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
+#[get("/health_check")]
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
 pub fn run(listener: TcpListener) -> std::io::Result<Server> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+    let server = HttpServer::new(|| App::new().service(health_check))
         .listen(listener)?
         .run();
     Ok(server)
