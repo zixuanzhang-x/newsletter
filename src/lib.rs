fa5b0fd0b829727a4598223f2cd738792e9b4fbd
Zixuan Zhang <zixuanzhang.x@gmail.com>
2023-11-14 21:36:28 -0500

    Add serde

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,30 @@
 use std::net::TcpListener;
 
 use actix_web::dev::Server;
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/health_check")]
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok()
+}
+
 pub fn run(listener: TcpListener) -> std::io::Result<Server> {
-    let server = HttpServer::new(|| App::new().service(health_check))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .service(health_check)
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
